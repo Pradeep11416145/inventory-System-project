a2c3fe13c32d3a9539ee775f372e3ee09aefcb5b
pradeepmaurya3976 <pradeepmaurya3976@gmail.com>
2024-03-10 07:25:58 -0700

    second commit

--- inventory-system/src/main/java/com/techsara/store/inventory/InventorySystemApplication.java
+++ inventory-system/src/main/java/com/techsara/store/inventory/InventorySystemApplication.java
@@ -12,7 +12,7 @@ public class InventorySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventorySystemApplication.class, args);
-		System.out.println("Application is running");
+		System.out.println("Applications is running");
 	}
 
 }
